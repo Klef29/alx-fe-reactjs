caf96b94110982d55ad76093dddbf4c015313e82
Klef29 <albertasare212@gmail.com>
2025-07-20 13:59:38 +0000

    Refactor Prop Drilling to Use Context API 1.02

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,19 +4,28 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import { UserContext } from './components/UserContext'
+import UserContext from './components/UserContext'
+   
 
 function App() {
-
+    const userData = {
+      name: "John Doe",
+      email: "ahdjhdbija212@gmail.com",
+    };  
   return (
+
     <>
       <div>
-        <WelcomeMessage />
+        {/* <WelcomeMessage />
         <Header />
         <MainContent />
+        
         <UserProfile name="John Doe" age={30} bio="New York, USA" />
         <UserProfile name="Jane Smith" age={25} bio="London, UK" />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> */}
+        <UserContext.Provider value={userData}>
+          <ProfilePage userData={userData} />
+        </UserContext.Provider>
       </div>
       <Footer/>
     </>
