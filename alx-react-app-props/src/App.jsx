e1aaf309b7dcaefdb979a94222be5215e0d1cb15
Klef29 <albertasare212@gmail.com>
2025-07-20 15:03:37 +0000

    Refactor Prop Drilling to Use Context API 1.04

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,35 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage'  
-import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import UserContext from './components/UserContext'
-   
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 function App() {
-    const userData = {
-      name: "John Doe",
-      email: "ahdjhdbija212@gmail.com",
-    };  
-  return (
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
 
-    <>
-      <div>
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <UserProfile name="John Doe" age={30} bio="New York, USA" />
-        <UserProfile name="Jane Smith" age={25} bio="London, UK" />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <UserContext.Provider value={userData}>
-          <ProfilePage userData={userData} />
-        </UserContext.Provider>
-      </div>
-      <Footer/>
-    </>
-  )
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
-
-export default App
+export default App;
