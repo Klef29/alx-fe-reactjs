e67d9c558f5ec7a7facde22f845a263ba1deb4d3
Klef29 <albertasare212@gmail.com>
2025-07-25 17:33:00 +0000

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,12 @@ import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+      searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter((recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())))
+  })),
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
@@ -12,6 +18,7 @@ const useRecipeStore = create((set) => ({
   updateRecipe: (updatedRecipe) =>
     set((state) => ({ recipes: state.recipes.map((recipe) => recipe.id === updatedRecipe.id ? updatedRecipe : recipe)   
     })),
+
 }));
 
 export default useRecipeStore;
