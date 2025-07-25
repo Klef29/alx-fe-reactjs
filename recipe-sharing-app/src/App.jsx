af59fbf6ad7b9628635a16ad20b28659e7bc4a48
Klef29 <albertasare212@gmail.com>
2025-07-25 09:02:19 +0000

    Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+
+  return (
+    <>
+    <RecipeList />
+    <AddRecipeForm />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
