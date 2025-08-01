0774a77fef41971966d5ab2feb971e7da094a7e7
Klef29 <albertasare212@gmail.com>
2025-08-01 17:50:23 +0000

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
+import SearchUsers from './components/Search.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,8 +15,10 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />  
-      <h1>App Component</h1>
-      <p>This is the main application component.</p>
+      <div>
+      <h2 style={{ textAlign: 'center', marginTop: '20px', color: 'blue' }}>Search User</h2>
+      <SearchUsers />
+    </div>
     </>
   )
 } 
--- github-user-search/src/components/Home.jsx
+++ github-user-search/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Welcome to the GitHub User Search App</h1>
+        <h1 style={{ textAlign: 'center', marginTop: '70px', }}>Welcome to the GitHub User Search App</h1>
     </div>
   )
 }
